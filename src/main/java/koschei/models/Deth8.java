ca0d28b309fd90fb8368f1b9057e5019e6a62836
Aleksandr Shablov <50119281+Protoff@users.noreply.github.com>
2021-02-18 17:20:35 +0300

    My decision

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,11 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    @Override
+    public String toString() {
+        return " - в общем все умерли.";
+    }
 }
